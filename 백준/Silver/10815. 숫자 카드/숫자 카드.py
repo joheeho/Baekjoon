bac54d06ff42df97203c76a8086c3693cef4f23d
joheeho <102297765+joheeho@users.noreply.github.com>
2023-01-17 00:14:43 +0900

    [Silver V] Title: 숫자 카드, Time: 476 ms, Memory: 118820 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,23 +1,13 @@
 import sys
 n = int(sys.stdin.readline().rstrip())
-data = list(map(int,sys.stdin.readline().rstrip().split()))
+data = set(map(int,sys.stdin.readline().rstrip().split()))
 m = int(sys.stdin.readline().rstrip())
 target_data = list(map(int,sys.stdin.readline().rstrip().split()))
-score=[]
-data.sort()
-def bin_search(data,x):
-    count=0
-    low=0
-    high=len(data)-1
-    while low<=high:
-        mid=(low+high)//2
-        if x==data[mid]:
-            count+=1
-            return count
-        elif x>data[mid]:
-            low=mid+1
-        else:
-            high=mid-1
-    return 0
-for i in range(len(target_data)):
-    print(bin_search(data,target_data[i]),end=' ')
+answer=[]
+for i in range(m):
+    if target_data[i] in data:
+        answer.append('1')
+    else:
+        answer.append('0')
+answer=' '.join(answer)
+print(answer)
