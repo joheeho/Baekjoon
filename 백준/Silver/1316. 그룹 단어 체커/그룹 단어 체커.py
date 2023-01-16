47a32939bfa67b2e89dfd9aa04d12c6bc51bc744
joheeho <102297765+joheeho@users.noreply.github.com>
2023-01-16 22:14:06 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 36 ms, Memory: 30748 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -4,21 +4,19 @@ for i in range(n):
     data=input() 
     if len(data)<=2:
         group_word+=1
-    else:                              
-        succession_word=1
+    else:
+        error=0
         for j in range(len(data)-1):
-            error=0
             if data[j] == data[j+1]:
-                succession_word+=1
-            else:
-                if data.count(data[j])==succession_word:
-                    succession_word=1
-                else:
-                    error=1
-                    break
+                continue
+            elif data[j] in data[j+1:]:
+                error=1
+                break
         if error ==0:
             group_word+=1
 
 print(group_word)
+
+            
 
             
