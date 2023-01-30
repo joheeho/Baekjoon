204a6d8c54c2d350bb918ea57a7e8c771da274e3
joheeho <102297765+joheeho@users.noreply.github.com>
2023-01-31 00:36:19 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 76 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,25 +1,21 @@
-N, M = map(int, input().split())
-original = []
-count = []
-
-for _ in range(N):
-    original.append(input())
-
-for a in range(N-7):
-    for b in range(M-7):
+n,m=map(int,input().split())
+board = [list(map(str, input())) for _ in range(n)]
+count=[]
+for a in range(n-7):          
+    for b in range(m-7):
         index1 = 0
         index2 = 0
         for i in range(a, a+8):
             for j in range(b, b+8):
                 if (i+j) % 2 == 0:
-                    if original[i][j] != 'W':
+                    if board[i][j] == 'W':
                         index1 += 1
-                    if original[i][j] != 'B':
+                    if board[i][j] == 'B':
                         index2 += 1
                 else:
-                    if original[i][j] != 'B':
+                    if board[i][j] == 'B':
                         index1 += 1
-                    if original[i][j] != 'W':
+                    if board[i][j] == 'W':
                         index2 += 1
         count.append(min(index1, index2))
 
