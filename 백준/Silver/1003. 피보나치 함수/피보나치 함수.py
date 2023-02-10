c728bfe809356ab1709937d78ae45880e637f029
joheeho <102297765+joheeho@users.noreply.github.com>
2023-02-10 17:52:35 +0900

    [Silver III] Title: 피보나치 함수, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -3,17 +3,12 @@ zero = [ 0 for i in range(41)]
 one =  [ 0 for i in range(41)]
 for i in range(t):
     n=int(input())
-    zero[0]=1
-    one[0]=0
-    zero[1]=0
-    one[1]=1
-    if n==1 or n==0:
-        print(zero[n],one[n])
-    else:
-        for i in range(2,n+1):
-            if (i <=n):
-                zero[i]=one[i-1]
-                one[i]=one[i-1]+one[i-2]
-            else:
-                break
-        print(zero[n],one[n])
+    for i in range(0,n+1):
+        zero[0]=1
+        one[0]=0
+        zero[1]=0
+        one[1]=1
+        if n>=2:
+            zero[i]=one[i-1]
+            one[i]=one[i-1]+one[i-2]
+    print(zero[n],one[n])
